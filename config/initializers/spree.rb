f024f1506584b841eddf42019fdb3afb44c1b318
Abhishek <abhishek.jain@vinsol.com>
2013-08-22 15:28:02 +0530

    add ship_station config

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -10,6 +10,21 @@ Spree.config do |config|
   # Example:
   # Uncomment to override the default site name.
   # config.site_name = "Spree Demo Site"
+    config.shipstation_username = "jainabhishek16@hotmail.com"
+    config.shipstation_password = "vinsol123"
+  # else
+  #   config.shipstation_username = "jody"
+  #   config.shipstation_password = "hull"
+  # end
+
+  config.shipstation_weight_units = "Grams" # Grams, Ounces or Pounds
+
+  # choose which number to send shipstation, use :shipment or :order, default is :shipment
+  config.shipstation_number = :shipment
+
+  # if you prefer to send notifications via shipstation
+  config.send_shipped_email = false
+
 end
 
 Spree.user_class = "Spree::User"
